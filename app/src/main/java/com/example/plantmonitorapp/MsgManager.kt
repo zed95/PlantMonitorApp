cdfee659a7e824c4b044c627c3876eb8ffce28ea
zed95 <zbigniew95@outlook.com>
2025-09-13 00:05:59 +0100

    packet stuffing/unstuffing and checksum calculation code

--- app/src/main/java/com/example/plantmonitorapp/MsgManager.kt
+++ app/src/main/java/com/example/plantmonitorapp/MsgManager.kt
@@ -2,10 +2,20 @@ package com.example.testproject
 
 import java.nio.ByteBuffer
 
-const val SOP: Byte = 0x00    // Start of packet identifier
+const val SOP: Byte = 0x7E    // Start of packet identifier
+const val SOP_ESCAPE: Byte = 0x7D
+const val SOP_STUFF: Byte  = 0x5E
+const val ESCAPE_STUFF: Byte = 0x5D
+const val  NON_SOP_BYTE: Byte = 0x7E
+const val NON_ESCAPE_BYTE: Byte = 0x7D
 const val wifiConnect: Byte = 0x01    // connect to wifi command identifier
 const val wifiConnectSts: Byte = 0x02
 const val testChecksum: Byte = 0x11
+
+// destuffing error codes
+const val ERR_BUF_OVERFLOW: Byte =   -2
+const val ERR_INVALID_ESCAPE: Byte = -3
+const val ERR_ESCAPE_AT_END: Byte =  -4
 
 fun msgConnectEsp32ToWifi(ssid: String, password: String): ByteArray
 {
@@ -53,4 +63,94 @@ fun bytesToInt(bytes: ByteArray, offset: Int): Int
     bytes[offset].toInt()
 
     return  result
+}
+
+/*
+* Byte in Kotlin is signed (–128..127). In C, uint8_t is unsigned (0..255).
+* That’s why we do (data[i].toInt() and 0xFF) → converts signed byte to an unsigned 0–255 value.
+* We keep sum as Int to avoid overflow while adding.
+* At the end, we apply two’s complement: ~sum + 1 → in Kotlin: (sum.inv() + 1).
+* & 0xFF ensures the result is clamped to 8 bits before casting back to Byte.
+* */
+fun calcChecksum(data: ByteArray, len: Int): Byte {
+    var sum = 0
+
+    for (i in 0 until len) {
+        sum = (sum + (data[i].toInt() and 0xFF)) and 0xFF
+    }
+
+    val checksum = ((sum.inv() + 1) and 0xFF)
+    return checksum.toByte()
+}
+
+
+fun stuffPacket(packet: ByteArray, len: Int): ByteArray
+{
+    val tmpBuf = mutableListOf<Byte>()
+
+    for(i in 0 until len)
+    {
+        if(packet[i] == SOP)
+        {
+            tmpBuf.add(SOP_ESCAPE)
+            tmpBuf.add(SOP_STUFF)
+        }
+        else if(packet[i] == SOP_ESCAPE)
+        {
+            tmpBuf.add(SOP_ESCAPE)
+            tmpBuf.add(ESCAPE_STUFF)
+        }
+        else
+        {
+            tmpBuf.add(packet[i])
+        }
+    }
+
+    return tmpBuf.toByteArray()
+}
+
+fun unstuffPacket(packet: ByteArray, len: Int): Pair<ByteArray?, Byte>
+{
+    val tmpBuf = mutableListOf<Byte>()
+    var errorCode: Byte = 0
+    var i = 0
+
+    while((i < len) && (errorCode == 0.toByte()))
+    {
+        if(packet[i] == SOP_ESCAPE)
+        {
+            // this is the last byte in the packet
+            if(i == (len - 1))
+            {
+                // report error
+                errorCode = ERR_ESCAPE_AT_END
+            }
+            else
+            {
+                if(packet[i + 1] == SOP_STUFF)
+                {
+                    tmpBuf.add(NON_SOP_BYTE)
+                    i++
+                }
+                else if(packet[i + 1] == ESCAPE_STUFF)
+                {
+                    tmpBuf.add(NON_ESCAPE_BYTE)
+                    i++
+                }
+                else
+                {
+                    errorCode = ERR_INVALID_ESCAPE
+                }
+            }
+        }
+        else
+        {
+            tmpBuf.add(packet[i])
+        }
+
+        i++
+    }
+
+    return if (errorCode == 0.toByte()) Pair(tmpBuf.toByteArray(), 0)
+    else Pair(null, errorCode)
 }
